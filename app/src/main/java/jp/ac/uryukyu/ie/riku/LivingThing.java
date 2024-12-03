00ae0bc3a44d5d7dfe4f12c3bced5b1a6c7aedb7
Riku Tamaki <e245734@ie.u-ryukyu.ac.jp>
2024-12-04 00:13:46 +0900

    report4

---
+++ app/src/main/java/jp/ac/uryukyu/ie/riku/LivingThing.java
@@ -0,0 +1,40 @@
+package jp.ac.uryukyu.ie.riku;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    public LivingThing(String name, int maximumHP, int attack){
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
+    }
+    public boolean isDead(){
+        return dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void attack(LivingThing opponent){
+        if (dead != true){
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+            }
+    }
+
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if (hitPoint <= 0) {
+            dead = true;
+            System.out.println("%sは倒れた");
+        }
+    }
+
+}
